255b4aee4b584cdb3362f0ec06ddc0f8da15f098
omarbougarne <omarbougarne98@gmail.com>
2026-06-02 18:31:55 +0100

    Addition of execution service and submission schema

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { ExercicesModule } from './exercices/exercices.module';
+import { SubmissiosModule } from './submissios/submissios.module';
+import { SubmissionsModule } from './submissions/submissions.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, ExercicesModule, SubmissiosModule, SubmissionsModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
