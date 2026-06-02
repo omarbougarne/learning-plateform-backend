7435d06bef437cba2b5eea1915af3aff326ca7e4
omarbougarne <omarbougarne98@gmail.com>
2026-06-02 19:47:32 +0100

    Setup final setup of docker

---
+++ dockerfile
@@ -0,0 +1,29 @@
+FROM node:22-alpine AS builder
+
+RUN npm install -g pnpm@9 && \
+    rm -rf /root/.cache/node/corepack
+
+WORKDIR /app
+
+COPY package.json pnpm-lock.yaml ./
+RUN pnpm install --frozen-lockfile
+
+COPY . .
+RUN pnpm build
+
+
+FROM node:22-alpine AS runner
+
+RUN npm install -g pnpm@9 && \
+    rm -rf /root/.cache/node/corepack
+
+WORKDIR /app
+
+COPY package.json pnpm-lock.yaml ./
+RUN pnpm install --frozen-lockfile
+
+COPY --from=builder /app/dist ./dist
+
+EXPOSE 3000
+
+CMD ["node", "dist/main"]
--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { ExercicesModule } from './exercices/exercices.module';
-import { SubmissiosModule } from './submissios/submissios.module';
 import { SubmissionsModule } from './submissions/submissions.module';
+import { ExecutionModule } from './execution/execution.module';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [AuthModule, ExercicesModule, SubmissiosModule, SubmissionsModule, UsersModule],
+    imports: [AuthModule, ExercicesModule, SubmissionsModule, SubmissionsModule, ExecutionModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
